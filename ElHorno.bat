393220d24b3ba825924c6a23c77219fec3e642e9
sergimol <sergimol@ucm.es>
2022-03-01 12:18:58 +0100

    Primeros bats

---
+++ ElHorno.bat
@@ -0,0 +1,25 @@
+@echo off
+cls
+set BAT=build.bat
+
+cd .\ElHorno\dependencies\
+
+cd .\Ogre\
+call %BAT%
+
+::cd ..\Bullet\
+::call %BAT%
+
+::cd ..\FMOD\
+::call %BAT%
+
+::cd ..\lua\
+::call %BAT%
+
+::cd ..\cegui\
+::call %BAT%
+
+cd ..\..
+msbuild "ElHorno.sln" /p:configuration=Debug
+msbuild "ElHorno.sln" /p:configuration=Release
+pause
---
+++ ElHorno/dependencies/Ogre/build.bat
@@ -0,0 +1,24 @@
+@echo off
+set EXES=.\..\..\..\Exes
+set CMAKE=.\..\..\cmake\bin\cmake.exe
+set SRC=..\Src
+
+mkdir .\Build
+cd .\Build
+
+%CMAKE% -DOGRE_BUILD_COMPONENT_BITES:BOOL="0"  -DOGRE_BUILD_RENDERSYSTEM_GL:BOOL="0" -DOGRE_INSTALL_DOCS:BOOL="0" -DOGRE_BUILD_PLUGIN_DOT_SCENE="0" -DOGRE_BUILD_RENDERSYSTEM_GL3PLUS:BOOL="1" -DOGRE_BUILD_PLUGIN_DOT_SCENE="0" -DOGRE_BUILD_RENDERSYSTEM_GL:BOOL="0" -DOGRE_BUILD_PLUGIN_DOT_SCENE="0" -DOGRE_BUILD_RENDERSYSTEM_GLES2:BOOL="0" -DOGRE_BUILD_PLUGIN_DOT_SCENE="0" -DOGRE_BUILD_RENDERSYSTEM_D3D11:BOOL="0" -DOGRE_BUILD_PLUGIN_DOT_SCENE="0" -DOGRE_BUILD_PLUGIN_ASSIMP:BOOL="0" -DOGRE_BUILD_PLUGIN_DOT_SCENE="0" -DOGRE_BUILD_SAMPLES:BOOL="0" -DOGRE_INSTALL_SAMPLES:BOOL="0" %SRC%
+msbuild "OGRE.sln" /p:configuration=Debug
+msbuild "OGRE.sln" /p:configuration=Release
+
+XCOPY /y /s .\bin\debug\OgreMain_d.dll %EXES%
+XCOPY /y /s .\bin\release\OgreMain.dll %EXES%
+
+XCOPY /y /s .\bin\debug\Codec_STBI_d.dll %EXES%
+XCOPY /y /s .\bin\release\Codec_STBI.dll %EXES%
+
+XCOPY /y /s .\bin\release\zlib.dll %EXES%
+
+XCOPY /y /s .\bin\debug\RenderSystem_GL3Plus_d.dll %EXES%
+XCOPY /y /s .\bin\release\RenderSystem_GL3Plus.dll %EXES%
+
+cd..
