8fd28f5286dc7df0cc304fb939ddaa24011c63cc
diecasti <diecasti@ucm.es>
2022-03-07 18:05:05 +0100

    inputManager Ok

--- ElHorno/Src/ElHornoBase/ElHornoInput/InputManager.cpp
+++ ElHorno/Src/ElHornoBase/ElHornoInput/InputManager.cpp
@@ -1,13 +1,8 @@
 #include "InputManager.h" //ajusten a ruta cuano organicemos carpetas
 #include <iostream>
 
-#include "SDL_keyboard.h"
-#include "SDL_keycode.h"
-#include <Vector>
-
-#include <SDL_events.h>
-#include <CEGUI/CEGUI.h>
-#include "checkML.h"
+//#include <CEGUI/CEGUI.h>
+//#include "checkML.h"
 
 InputManager* InputManager::instance = 0;
 
@@ -18,10 +13,10 @@ InputManager::InputManager()
 
 InputManager::~InputManager()
 {
-	for (int i = 0; i < NumControls && controllers.size() > i; i++) {
+	/*for (int i = 0; i < NumControls && controllers.size() > i; i++) {
 		SDL_GameControllerClose(controllers[i].cReference);
 	}
-	controllers.clear();
+	controllers.clear();*/
 }
 
 InputManager* InputManager::getInstance()
@@ -55,19 +50,17 @@ void InputManager::clean()
 void InputManager::setup()
 {
 
-	//TODO
+	//TODO configuracion de mandos
 }
 
 //recoge los principales eventos de SDL y los gestiona de ser necesario
 void InputManager::GeneralInputManagement(SDL_Event event)
 {
-	//casa paco sponsored
-
 
 	switch (event.type)
 	{
 		//En principio, como se puede preguntar si X tecla o boton del mando está pulsada/o, no hace falta gestionar esos eventos
-		//Teclado
+		//Teclado y mando
 
 		//Estos si hay que guardarlos
 		//Raton
@@ -75,10 +68,10 @@ void InputManager::GeneralInputManagement(SDL_Event event)
 
 		break;
 	case SDL_MOUSEWHEEL:
-		if (event.wheel.y != mouseWheel.y) // scroll up/down
+		if (event.wheel.y) // scroll up/down
 		{
 		}
-		else if (event.wheel.x != mouseWheel.x) // scroll left
+		else if (event.wheel.x) // scroll left
 		{
 		}
 		break;
@@ -118,9 +111,9 @@ void InputManager::GeneralInputManagement(SDL_Event event)
 }
 
 
-bool InputManager::IsKeyDown(Scancode key)
+bool InputManager::IsKeyDown(SDL_Scancode key)
 {
 	const Uint8* state = SDL_GetKeyboardState(NULL);
 
-	return state[(SDL_Scancode)key];
+	return state[key];
 }
