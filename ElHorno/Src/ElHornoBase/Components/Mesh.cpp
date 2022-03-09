99c5e9e84e0b0d4af088df454293825894b23698
Sergio Alfonso <sergialf@ucm.es>
2022-03-09 18:49:06 +0100

    Completado componente Mesh
    Co-authored-by: diecasti<diecasti@ucm.es>
    Co-authored-by: javimu06 <javimu06@ucm.es>

--- ElHorno/Src/ElHornoBase/Components/Mesh.cpp
+++ ElHorno/Src/ElHornoBase/Components/Mesh.cpp
@@ -1,95 +1,108 @@
-//#include "Mesh.h"
-//#include "../ElHornoBase.h"; 
-//
-//
-//#include "OgreSceneManager.h"
-//#include "OgreEntity.h"
-//#include "../ECS/Entity.h"
-//#include "Transform.h"
-//
-//Mesh::Mesh(nlohmann::json& args) : Component() {}
-//
-//Mesh::~Mesh()
-//{
-//	Ogre::SceneNode* sn = ogreEntity_->getParentSceneNode();
-//	if (sn != nullptr)
-//		sn->detachAllObjects();
-//	ElHornoBase::getInstance()->getSceneManager()->destroyEntity(ogreEntity_->getName());
-//}
-//
-//void Mesh::init(nlohmann::json& args)
-//{
-//	std::string cast = args["mesh"];
-//	meshFile_ = cast;
-//	ogreEntity_ = ElHornoBase::getInstance()->getSceneManager()->createEntity(meshFile_);
-//	castShadow_ = true;
-//
-//	if (!args["material"].is_null())
-//	{
-//		std::string aux = args["material"];
-//		materialName_ = aux;
-//		ogreEntity_->setMaterialName(args["material"], "Materials");
-//	}
-//
-//	if (!args["castShadow_"].is_null())
-//		castShadow_ = args["castShadow_"];
-//
-//	isAnimated_ = ((!args["isAnimated_"].is_null()) && (args["isAnimated_"]));
-//
-//	ogreEntity_->setCastShadows(castShadow_);
-//	entity_->getComponent<Transform>("Transform")->getNode()->attachObject(ogreEntity_);
-//}
-//
-//void Mesh::redefine(nlohmann::json& args)
-//{
-//	entity_->getComponent<Transform>("Transform")->getNode()->detachObject(ogreEntity_);
-//
-//	delete ogreEntity_;
-//	ogreEntity_ = nullptr;
-//
-//	if (args["mesh"].is_null())
-//		args["mesh"] = meshFile_;
-//
-//	if (args["material"].is_null())
-//		args["material"] = materialName_;
-//
-//	if (args["isAnimated_"].is_null())
-//		args["isAnimated_"] = isAnimated_;
-//
-//	init(args);
-//}
-//
-//void Mesh::onEnable()
-//{
-//	ogreEntity_->setVisible(true);
-//}
-//
-//void Mesh::onDisable()
-//{
-//	ogreEntity_->setVisible(false);
-//}
-//
-//Ogre::AxisAlignedBox Mesh::getAABB() const
-//{
-//	return ogreEntity_->getBoundingBox();
-//}
-//
-////Ogre::Real Mesh::getAABBRadius() const
-////{
-////	return ogreEntity_->getBoundingRadius();
-////}
-//
-//Ogre::Entity* Mesh::getOgreEntity() const
-//{
-//	return ogreEntity_;
-//}
-//
-//Ogre::Mesh* Mesh::getMesh() const
-//{
-//	return ogreEntity_->getMesh().get();
-//}
-//
-//bool Mesh::isMeshAnimated() const
-//{
-//	return isAnimated_;
-//}
+#include "../ElHornoBase.h"; 
+#include "OgreSceneManager.h"
+#include "OgreEntity.h"
+#include "../ECS/Entity.h"
+#include "Mesh.h"
+#include "Transform.h"
+
+Mesh::Mesh(nlohmann::json& args) : Component() {}
+
+/*
+Desconecta el nodo del padre y destruye la entidad
+*/
+Mesh::~Mesh()
+{
+	Ogre::SceneNode* node = ogreEntity_->getParentSceneNode();
+	if (node != nullptr)
+		node->detachAllObjects();
+	ElHornoBase::getInstance()->getSceneManager()->destroyEntity(ogreEntity_->getName());
+}
+
+/*
+Coge desde un json la malla y crea una entidad. Establece:
+	- Material
+	- Casteo de sombras
+	- Animación
+Se le adjunta al nodo del transform
+*/
+void Mesh::init(nlohmann::json& args)
+{
+	meshName_ = to_string(args["mesh"]);
+	ogreEntity_ = ElHornoBase::getInstance()->getSceneManager()->createEntity(meshName_);
+	castShadow_ = true;
+
+	if (!args["material"].is_null())
+	{
+		std::string aux = args["material"];
+		materialName_ = aux;
+		ogreEntity_->setMaterialName(args["material"], "Materials");
+	}
+
+	if (!args["castShadow_"].is_null())
+		castShadow_ = args["castShadow_"];
+
+	isAnimated_ = ((!args["isAnimated_"].is_null()) && (args["isAnimated_"]));
+
+	ogreEntity_->setCastShadows(castShadow_);
+	entity_->getComponent<Transform>("Transform")->getNode()->attachObject(ogreEntity_);
+}
+
+/*
+Reestablece varibles de init para recomponer la malla
+*/
+void Mesh::redefine(nlohmann::json& args)
+{
+	entity_->getComponent<Transform>("Transform")->getNode()->detachObject(ogreEntity_);
+
+	delete ogreEntity_;
+	ogreEntity_ = nullptr;
+
+	if (args["mesh"].is_null())
+		args["mesh"] = meshName_;
+
+	if (args["material"].is_null())
+		args["material"] = materialName_;
+
+	if (args["isAnimated_"].is_null())
+		args["isAnimated_"] = isAnimated_;
+
+	init(args);
+}
+
+void Mesh::onEnable()
+{
+	ogreEntity_->setVisible(true);
+}
+
+void Mesh::onDisable()
+{
+	ogreEntity_->setVisible(false);
+}
+
+Ogre::AxisAlignedBox Mesh::getAABB() const
+{
+	return ogreEntity_->getBoundingBox();
+}
+
+Ogre::Real Mesh::getAABBRadius() const
+{
+	return ogreEntity_->getBoundingRadius();
+}
+
+Ogre::Entity* Mesh::getOgreEntity() const
+{
+	return ogreEntity_;
+}
+
+/*
+Devuelve ptr de SharedPtr<Mesh>
+*/
+Ogre::Mesh* Mesh::getMesh() const
+{
+	return ogreEntity_->getMesh().get();
+}
+
+bool Mesh::isMeshAnimated() const
+{
+	return isAnimated_;
+}
--- ElHorno/Src/ElHornoBase/Components/Mesh.h
+++ ElHorno/Src/ElHornoBase/Components/Mesh.h
@@ -1,46 +1,45 @@
-//#pragma once
-//#ifndef _MESH_H
-//#define _MESH_H
-//
-//#include <string>
-//#include "../json.hpp"
-//#include "../ECS/Component.h"
-////class Component;
-//
-//namespace Ogre {
-//	class AxisAlignedBox;
-//	class Real;
-//	class Entity;
-//	class Mesh;
-//}
-//
-//class Mesh : public Component
-//{
-//public:
-//
-//	Mesh(nlohmann::json& args);
-//	~Mesh();
-//
-//	void init(nlohmann::json& args);
-//	void redefine(nlohmann::json& args);
-//
-//	void onEnable() override;
-//	void onDisable() override;
-//
-//	//Metodos de Ogre
-//	Ogre::AxisAlignedBox getAABB() const;
-//	//Ogre::Real getAABBRadius() const;
-//	Ogre::Entity* getOgreEntity() const;
-//	Ogre::Mesh* getMesh() const;
-//
-//	bool isMeshAnimated() const;
-//private:
-//	Ogre::Entity* ogreEntity_;
-//	bool isAnimated_;
-//	bool castShadow_;
-//
-//	Ogre::String meshFile_;
-//	Ogre::String materialName_;
-//};
-//
-//#endif _MESH_H
+#pragma once
+#ifndef _MESH_H
+#define _MESH_H
+
+#include <string>
+#include "../json.hpp"
+#include "../ECS/Component.h"
+
+namespace Ogre {
+	class AxisAlignedBox;
+	class Entity;
+	class Mesh;
+}
+
+class Mesh : public Component
+{
+public:
+
+	Mesh(nlohmann::json& args);
+	~Mesh();
+
+	void init(nlohmann::json& args);
+	void redefine(nlohmann::json& args);
+
+	void onEnable() override;
+	void onDisable() override;
+
+	float getAABBRadius() const;
+
+	//Metodos de Ogre
+	Ogre::AxisAlignedBox getAABB() const;
+	Ogre::Entity* getOgreEntity() const;
+	Ogre::Mesh* getMesh() const;
+
+	bool isMeshAnimated() const;
+private:
+	Ogre::Entity* ogreEntity_;
+	bool isAnimated_;
+	bool castShadow_;
+
+	std::string meshName_;
+	std::string materialName_;
+};
+
+#endif _MESH_H
