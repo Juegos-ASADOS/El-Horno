f94ff9496bb540f0b2f3ec53967df218db909b7a
Samuebla <Samuebla@ucm.es>
2022-03-22 10:44:16 +0100

    Continuacion Manager de CEGUI

--- ElHorno/Src/ElHornoCEGUI/UIManager.cpp
+++ ElHorno/Src/ElHornoCEGUI/UIManager.cpp
@@ -1,9 +1,20 @@
 #include "UIManager.h"
+#include "UIElement.h"
+
+#include <GraphicsManager.h>
+#include <CEGUI/CEGUI.h>
+#include <CEGUI/RendererModules/Ogre/Renderer.h>
+
+#include <OgreRenderWindow.h>
 
 UIManager* UIManager::instance = 0;
 
-UIManager::UIManager()
+UIManager::UIManager(Ogre::RenderWindow* renderWindow)
 {
+	CEGUI::OgreRenderer& auxiliar = CEGUI::OgreRenderer::bootstrapSystem(*GraphicsManager::getInstance()->getRenderWindow());
+	renderer = &auxiliar;
+	createRoot();
+
 	
 }
 
@@ -17,11 +28,11 @@ UIManager* UIManager::getInstance()
 }
 
 
-bool UIManager::setupInstance()
+bool UIManager::setupInstance(Ogre::RenderWindow* window)
 {
 	if (instance == 0)
 	{
-		instance = new UIManager();
+		instance = new UIManager(window);
 		return true;
 	}
 
@@ -31,4 +42,80 @@ bool UIManager::setupInstance()
 void UIManager::clean()
 {
 	delete instance;
+}
+
+void UIManager::createRoot()
+{
+}
+
+void UIManager::changeScreenSize(int width, int height)
+{
+	CEGUI::System::getSingleton().getRenderer()->setDisplaySize(CEGUI::Size<float>(width, height));
+}
+
+void UIManager::update()
+{
+}
+
+void UIManager::changeText(CEGUI::Window* window, std::string text)
+{
+	window->setText(text);
+}
+
+//Para cambiar el ratón
+void UIManager::setMouseCursor(const std::string& imageFile)
+{
+	CEGUI::System::getSingleton().getDefaultGUIContext().getMouseCursor().setDefaultImage(imageFile);
+
+}
+
+void UIManager::showMouseCursor()
+{
+	CEGUI::System::getSingleton().getDefaultGUIContext().getMouseCursor().show();
+
+}
+
+void UIManager::hideMouseCursor()
+{
+	CEGUI::System::getSingleton().getDefaultGUIContext().getMouseCursor().hide();
+}
+
+UIElement* UIManager::getRoot()
+{
+	return root;
+}
+
+CEGUI::Window* UIManager::getWindow(const std::string& name )
+{
+	//Si no fuciona intentar static_Cast
+	return root->getWindowElement()->getChild(name);
+}
+
+CEGUI::WindowManager* UIManager::getWinMngr()
+{
+	return winMngr;
+}
+
+void UIManager::setVisibleLayout(bool visible, int layout)
+{
+	layouts[layout]->setVisible(visible);
+}
+
+void UIManager::changeAlpha(const std::string & imagePath, float alpha)
+{
+	//Si sale error hacer static_cast
+	CEGUI::Window* thumbnail = root->getWindowElement()->getChild(imagePath);
+	thumbnail->setAlpha(alpha);
+}
+
+float UIManager::getAlpha(const std::string & namePath )
+{
+	//Si sale error hacer static_cast
+	CEGUI::Window* thumbnail = root->getWindowElement()->getChild(namePath);
+	return thumbnail->getAlpha();
+}
+
+std::vector<CEGUI::Window*> UIManager::getLayouts()
+{
+	return layouts;
 }
