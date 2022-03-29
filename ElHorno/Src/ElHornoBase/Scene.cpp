9d5762fe6647dda957ad23e53fb9db57d0fb91a6
sergimol <sergimol@ucm.es>
2022-03-29 11:40:48 +0200

    Arreglos en AudioManager

--- ElHorno/Src/ElHornoBase/Scene.cpp
+++ ElHorno/Src/ElHornoBase/Scene.cpp
@@ -9,6 +9,8 @@
 #include "LightComponent.h"
 #include "Mesh.h"
 #include "Rigibody.h"
+#include "AudioComponent.h"
+#include "AudioListenerComponent.h"
 
 using json = nlohmann::json;
 
@@ -22,11 +24,13 @@ Scene::Scene()
 	Ogre::Vector3 p = { 1,1,1};
 	a->addComponent<Transform>("transform", Ogre::Vector3(0,0,0), Ogre::Vector3(0, 0, 0), Ogre::Vector3(0, 0, 0));
 	a->addComponent<CameraComponent>("camera", Ogre::Vector3(0,220,550), Ogre::Vector3(0, 0, 0), Ogre::ColourValue(0,0.3,0.5), 5, 10000);
+	a->addComponent<AudioListenerComponent>("audioListener");
 	
 	Entity* b = addEntity("object", "prueba");
 	b->addComponent<Transform>("transform", Ogre::Vector3(0, 50, 0), Ogre::Vector3(180, 0, 0), p);
 	b->addComponent<Mesh>("mesh", "ogrehead");
 	b->addComponent<RigidBody>("rigidbody", 2.0f, false, 0);
+	b->addComponent<AudioComponent>("audioComponent");
 
 	b = addEntity("object2", "prueba");
 	b->addComponent<Transform>("transform", Ogre::Vector3(0, -15, 0), Ogre::Vector3(0, 0, 0), Ogre::Vector3(5, 0.1, 5));
