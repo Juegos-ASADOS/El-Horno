810e63fc1c8ea144293268a8d979017c0fa853ff
albemu07 <albemu07@ucm.es>
2022-03-30 20:25:04 +0200

    path y file extension como define

--- ElHorno/Src/ElHornoLua/LuaManager.h
+++ ElHorno/Src/ElHornoLua/LuaManager.h
@@ -2,6 +2,9 @@
 #ifndef _EL_HORNO_BULLET_LUA_MANAGER_H
 #define _EL_HORNO_BULLET_LUA_MANAGER_H
 
+#define SCRIPTS_PATH "\Assets\Scripts"
+#define FILE_EXTENSION ".lua"
+
 #include <string>
 
 class lua_State;
