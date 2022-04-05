6f020fe5f0d5cc24fabc37ada7011f6cf1c0af2a
acarni01 <acarni01@ucm.es>
2022-04-05 02:51:11 +0200

    Personaje en escena con animaciones

--- ElHorno/Src/ElHornoBase/Scene.cpp
+++ ElHorno/Src/ElHornoBase/Scene.cpp
@@ -22,45 +22,32 @@
 namespace El_Horno {
 	Scene::Scene()
 	{
-		/*Entity* light = addEntity("light", "prueba");
-		light->addComponent<Transform>("transform", Ogre::Vector3(0, 200, 0), Ogre::Vector3(0, 0, 0), Ogre::Vector3(0, 0, 0));
-		light->addComponent<LightComponent>("light", 0, Ogre::Vector3(0, 0, 0));
+		Entity* light = addEntity("light", "prueba");
+		light->addComponent<Transform>("transform", HornoVector3(0, 200, 200), HornoVector3(0, 0, 0), HornoVector3(0, 0, 0));
+		light->addComponent<LightComponent>("light", 0, HornoVector3(0, 0, 0));
 
 		Entity* a = addEntity("camera", "prueba");
-		Ogre::Vector3 p = { 10,10,10 };
-		a->addComponent<Transform>("transform", Ogre::Vector3(0, 0, 0), Ogre::Vector3(0, 0, 0), Ogre::Vector3(0, 0, 0));
-		a->addComponent<CameraComponent>("camera", Ogre::Vector3(0, 600, 100), Ogre::Vector3(0, 0, 0), Ogre::ColourValue(0, 0.3, 0.5), 5, 10000);
+		El_Horno::HornoVector3 p = { 10,10,10 };
+		a->addComponent<Transform>("transform", El_Horno::HornoVector3(0, 0, 0), El_Horno::HornoVector3(0, 0, 0), El_Horno::HornoVector3(0, 0, 0));
+		a->addComponent<CameraComponent>("camera", El_Horno::HornoVector3(0, 100, 450), El_Horno::HornoVector3(0, 0, 0), El_Horno::HornoVector3(0, 0.3, 0.5), 1, 5, 10000);
 		a->addComponent<AudioListenerComponent>("audioListener");
 
 		Entity* b = addEntity("object", "prueba");
-		b->addComponent<Transform>("transform", Ogre::Vector3(0, 150, 0), Ogre::Vector3(0, 0, 0), p);
-		b->addComponent<Mesh>("mesh", "Sinbad");
+		b->addComponent<Transform>("transform", El_Horno::HornoVector3(0, 10, 0), El_Horno::HornoVector3(0, 0, 0), p);
+		b->addComponent<Mesh>("mesh", "pipoTeOdio");
 		b->addComponent<RigidBody>("rigidbody", 2.0f, false, false, 0);
 		b->addComponent<AnimatorController>("animatorController");
 		b->addComponent<SinbadAnimTest>("sinbadAnimTest");
 		b->addComponent<AudioComponent>("audioComponent");
 
 		b = addEntity("object2", "prueba");
-		b->addComponent<Transform>("transform", Ogre::Vector3(0, -15, 0), Ogre::Vector3(0, 0, 0), Ogre::Vector3(5, 0.1, 5));
+		b->addComponent<Transform>("transform", El_Horno::HornoVector3(0, -15, 0), El_Horno::HornoVector3(0, 0, 0), El_Horno::HornoVector3(5, 0.1, 5));
 		b->addComponent<Mesh>("mesh", "cube");
 		b->addComponent<RigidBody>("rigidbody", 0.0f, false, false, 0);
 
 		b = addEntity("esmoque", "prueba");
-		b->addComponent<Transform>("transform", Ogre::Vector3(250, 10, 0), Ogre::Vector3(0, 0, 0), p/10);
-		b->addComponent<ParticleSystem>("particleSystem", "smoke", "Smoke", 10, true);*/
-
-		//// Sinbad cuidao ogro
-		//Entity* c = addEntity("ogro", "prueba");
-		//c->addComponent<Transform>("transform", Ogre::Vector3(100, 50, 0), Ogre::Vector3(0, 0, 0), Ogre::Vector3(10, 10, 10));
-		//c->addComponent<Mesh>("mesh", "Sinbad");
-		//c->addComponent<AnimatorController>("animatorController");
-		//c->addComponent<SinbadAnimTest>("sinbadAnimTest");
-		//// doble sinbad de combate
-		//Entity* d = addEntity("ogro", "prueba");
-		//d->addComponent<Transform>("transform", Ogre::Vector3(-100, 50, 0), Ogre::Vector3(0, 0, 0), Ogre::Vector3(10, 10, 10));
-		//d->addComponent<Mesh>("mesh", "Sinbad");
-		//d->addComponent<AnimatorController>("animatorController");
-		//d->addComponent<SinbadAnimTest>("sinbadAnimTest");
+		b->addComponent<Transform>("transform", El_Horno::HornoVector3(250, 10, 0), El_Horno::HornoVector3(0, 0, 0), p);
+		b->addComponent<ParticleSystem>("particleSystem", "smoke", "Smoke", 10, true);
 		
 	}
 
