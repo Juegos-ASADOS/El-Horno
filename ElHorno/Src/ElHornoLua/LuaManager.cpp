7d51ed6056c918a716684080c088950aa44b5b9d
albemu07 <albemu07@ucm.es>
2022-04-21 00:28:13 +0200

    Cambios en el LuaManager

--- ElHorno/Src/ElHornoLua/LuaManager.cpp
+++ ElHorno/Src/ElHornoLua/LuaManager.cpp
@@ -23,107 +23,104 @@ extern "C"
 
 namespace El_Horno {
 
-LuaManager* LuaManager::instance_;
+    LuaManager* LuaManager::instance_;
 
-LuaManager* LuaManager::getInstance()
-{
-    if (instance_ == 0)
-        return nullptr;
+    LuaManager* LuaManager::getInstance()
+    {
+        if (instance_ == 0)
+            return nullptr;
 
-    return instance_;
-}
-
-bool LuaManager::setupInstance()
-{
-    if (instance_ == 0) {
-        instance_ = new LuaManager();
-        return true;
-    }
-    return false;
-}
-
-void LuaManager::erase()
-{  
-    delete instance_;
-}
-
-void LuaManager::init()
-{
-    // create a Lua state
-   luaState = luaL_newstate();
-
-    // load standard libs
-    luaL_openlibs(luaState);
-
-    luabridge::getGlobalNamespace(luaState)
-        .beginClass<Example>("Example")
-        .addConstructor<void(*) (const std::string& name)>()
-        .addFunction("printName", &(Example::printName))
-        .endClass();
-
-    exposeEntity();
-}
-
-void LuaManager::report_errors(int status)
-{
-    if (status == 0) {
-        return;
+        return instance_;
     }
 
-    std::cerr << "[LUA ERROR] " << lua_tostring(luaState, -1) << std::endl;
+    bool LuaManager::setupInstance()
+    {
+        if (instance_ == 0) {
+            instance_ = new LuaManager();
+            return true;
+        }
+        return false;
+    }
 
-    // remove error message from Lua state
-    lua_pop(luaState, 1);
-}
+    void LuaManager::erase()
+    {
+        delete instance_;
+    }
 
-void LuaManager::reedLuaScript(const std::string& path)
-{
-    // load some code from Lua file
-    int scriptLoadStatus = luaL_dofile(luaState, path.c_str());
+    void LuaManager::init()
+    {
+        // create a Lua state
+        luaState = luaL_newstate();
 
-    // define error reporter for any Lua error
-    report_errors(scriptLoadStatus);
-}
+        // load standard libs
+        luaL_openlibs(luaState);
+    }
 
-luabridge::LuaRef LuaManager::getFromLua(std::string name)
-{
-    std::string s = SCRIPTS_PATH + name + FILE_EXTENSION;
-    return luabridge::getGlobal(luaState, s.c_str());
-}
+    void LuaManager::report_errors(int status)
+    {
+        if (status == 0) {
+            return;
+        }
 
-template<typename T>
-void LuaManager::pushToLua(T var, std::string name) {
-    luabridge::push(luaState, var);
-    lua_setglobal(luaState, name.c_str());
-}
+        std::cerr << "[LUA ERROR] " << lua_tostring(luaState, -1) << std::endl;
 
-LuaManager::~LuaManager()
-{
-    lua_close(luaState);
-}
+        // remove error message from Lua state
+        lua_pop(luaState, 1);
+    }
 
-void LuaManager::exposeEntity()
-{
-    luabridge::getGlobalNamespace(luaState)
-        .beginClass<Entity>("Entity")
-        .addConstructor<void(*) (std::string n, Scene* m, Entity* p)>()
-        .addConstructor<void(*) (Scene* m)>()
-        //.addFunction("addComponent", &(Entity::addComponent<>))
-        .addFunction("setParent", &(Entity::setParent))
-        .addFunction("addChild", &(Entity::addChild))
-        .addFunction("setActive", &(Entity::setActive))
-        .endClass();
-}
+    void LuaManager::reedLuaScript(const std::string& path)
+    {
+        // load some code from Lua file
+        int scriptLoadStatus = luaL_dofile(luaState, path.c_str());
 
-std::string Example::getName() const
-{
-	return m_name;
-}
+        // define error reporter for any Lua error
+        report_errors(scriptLoadStatus);
+    }
 
-void Example::printName()
-{
-	std::cout << "[C++ CODE] Hello, my name is " << m_name << "!" << "\n";
-}
+    luabridge::LuaRef LuaManager::getFromLua(std::string name)
+    {
+        std::string s = SCRIPTS_PATH + name + FILE_EXTENSION;
+        return luabridge::getGlobal(luaState, s.c_str());
+    }
+
+    lua_State* LuaManager::getLuaState()
+    {
+        return luaState;
+    }
+
+    void LuaManager::exposeFunct(std::string name, lua_CFunction(*func)(lua_State* L))
+    {
+        lua_register(luaState, name.c_str(), func(luaState));
+    }
+
+    int LuaManager::luaGetTop(lua_State* L)
+    {
+        return lua_gettop(L);
+    }
+
+    template<typename T>
+    void LuaManager::pushToLua(T var, std::string name) {
+        luabridge::push(luaState, var);
+        lua_setglobal(luaState, name.c_str());
+    }
+
+    LuaManager::~LuaManager()
+    {
+        lua_close(luaState);
+    }
+
+    void LuaManager::exposeEntity()
+    {
+        luabridge::getGlobalNamespace(luaState)
+            .beginClass<Entity>("Entity")
+            .addConstructor<void(*) (std::string n, Scene* m, Entity* p)>()
+            .addConstructor<void(*) (Scene* m)>()
+            //.addFunction("addComponent", &(Entity::addComponent<>))
+            .addFunction("setParent", &(Entity::setParent))
+            .addFunction("addChild", &(Entity::addChild))
+            .addFunction("setActive", &(Entity::setActive))
+            .endClass();
+    }
 }
 // create a global variable (an instance_ of a Greeter class) in Lua scope
 //auto globalGreeter = std::make_unique<Example>("noname");
