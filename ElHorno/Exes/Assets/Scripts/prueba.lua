8daae03579f73548970723415c281506607a21e0
albemu07 <albemu07@ucm.es>
2022-04-29 10:42:16 +0200

    creando escena en lua

--- ElHorno/Exes/Assets/Scripts/prueba.lua
+++ ElHorno/Exes/Assets/Scripts/prueba.lua
@@ -1,13 +1,148 @@
-prueba = {
+gamemanager = {
 	transform = {
-        position = " -50 ,40 ,0 ",
+        position = " 0 ,0 ,0 ",
         rotation = " 0 ,0 ,0 ",
-        scale = " 10 ,10 ,10 ",
+        scale = " 0 ,0 ,0 ",
+    },
+
+    gamemanager = {      
+    },
+}
+
+light = {
+    transform = {
+        position = " 0 ,200 ,200 ",
+        rotation = " 0 ,0 ,0 ",
+        scale = " 0 ,0 ,0 ",
+    },
+
+    light = {
+        type = "0",
+        dirLight = = " 0 ,0 ,0 ",
+    },
+}
+
+camera = {
+    transform = {
+        position = " 0 ,0 ,0 ",
+        rotation = " 0 ,0 ,0 ",
+        scale = " 0 ,0 ,0 ",
+    },
+
+    camera = {
+        position = " 0, 100, 450 ",
+        lookAt = " 0, 0, 0 ",
+        color = " 0, 0.3, 0.5, 1 ",
+        nearClDis = "5",
+        farClDis = "10000",
+    },
+
+    audioListener = {
+
+    },
+}
+
+player = {
+    transform = {
+        position = " -50, 40, 0 ",
+        rotation = " 0 ,0 ,0 ",
+        scale = " 10,10,10 ",
     },
 
     mesh = {
-         name = "pipo",
+        name = "pipo",
+    },
+
+    rigidbody = {
+        mass = "2",
+        isTrigger = "0",
+        isKinematic = "0",
+        colShape = "0",
+    },
+
+    audioComponent = {
+
+    },
+
+    playerController = {
+        speed = "1500",
+        maxForce = "200",
+    },
+
+    playerInteract = {
+
+    },
+
+    animatorController = {
+        Idle = "walk",
+		walk = "Idle",
+		Idle_with_product = "walk_with_product",
+		walk_with_product = "Idle_with_product",
+		Idle_with_cart = "walk_with_cart",
+		walk_with_cart = "Idle_with_cart",
+		AnyState = "Idle",
+		AnyState = "Idle_with_product",
+		AnyState = "Idle_with_cart",
+    },
+}
+
+object = {
+    transform = {
+        position = " 0, -15, 0 ",
+        rotation = " 0 ,0 ,0 ",
+        scale = " 15, 0.1, 15 ",
+    },
+
+    mesh = {
+        name = "cube",
+    },
+
+    rigidbody = {
+        mass = "0",
+        isTrigger = "0",
+        isKinematic = "0",
+        colShape = "0",
+    },
+}
+
+estantery = {
+    transform = {
+        position = " 50, 10, 0 ",
+        rotation = " 0 ,0 ,0 ",
+        scale = " 0.5, 1, 0.25 ",
+    },
+
+    mesh = {
+        name = "cube",
+    },
+
+    rigidbody = {
+        mass = "0",
+        isTrigger = "0",
+        isKinematic = "0",
+        colShape = "0",
+    },
+}
+
+estanteryTrigger = {
+    transform = {
+        position = " 0, 0, 0 ",
+        rotation = " 0, 0, 0 ",
+        scale = " 5, 5, 5 ",
+    },
+
+    rigidbody = {
+        mass = "0",
+        isTrigger = "1",
+        isKinematic = "0",
+        colShape = "0",
+    },
+
+    entityId = {
+
     }
 }
 
-entities = {"prueba"}
+
+
+entities = {"gamemanager"}
